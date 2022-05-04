1ea052d91b53f63757b19d83b3c3361f8614249d
Tama <tamawish@gmail.com>
2022-05-04 16:54:19 +0900

    ホームblade作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::get('/', [HomeController::class, 'index']);
