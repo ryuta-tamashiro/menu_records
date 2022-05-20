ac1ad35e28648c3aa4b9fda17b8bccd8e5bc591c
Tama <tamawish@gmail.com>
2022-05-20 16:01:42 +0900

    共通レイアウト作成・管理ユーザー一覧画面作成

---
+++ src/resources/views/member/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.layout')
+@section('title', 'Tutrial for beginner')
+@section('content')
+<div class="page-header" style="margin-top:-30px;padding-bottom:0px;">
+  <h1><small>ホーム</small></h1>
+</div>
+@endsection
--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UsersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,8 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/users', [UsersController::class, 'index']);
